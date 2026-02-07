475d57a8149a74153bb9263adc744b6496cf7ea0
liuboping <liuboping@elex-tech.com>
2026-02-07 16:42:41 +0800

    Fix username

--- Casks/lbpssh.rb
+++ Casks/lbpssh.rb
@@ -2,10 +2,10 @@ cask "lbpssh" do
   version "1.0.1"
   sha256 :no_check
 
-  url "https://github.com/lbp008/lbpssh/releases/download/v#{version}/lbpSSH-macos-universal.zip"
+  url "https://github.com/lbp0200/lbpssh/releases/download/v#{version}/lbpSSH-macos-universal.zip"
   name "lbpSSH"
   desc "Cross-platform SSH terminal manager"
-  homepage "https://github.com/lbp008/lbpssh"
+  homepage "https://github.com/lbp0200/lbpssh"
 
   app "lbpSSH.app"
 
